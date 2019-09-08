0fdad9ebc17a67d2e18aff0dd609c00ae8fa99f1
藤原靖浩 <fujiwara397@gmail.com>
2019-09-08 21:56:35 +0900

    ScriptTestプロジェクトのコミット テックアカデミーの課題

---
+++ Assets/hairetu2.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class hairetu2 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {// 要素数5の配列を初期化する
+		int[] arrays = new int[5];
+
+		// 配列の各要素に値を代入する
+		arrays [0] = 100;
+		arrays [1] = 300;
+		arrays [2] = 500;
+		arrays [3] = 700;
+		arrays [4] = 900;
+
+		// 配列の要素をすべて表示する
+		for (int i = 4; i >=0; i--) {
+			Debug.Log (arrays [i]);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
